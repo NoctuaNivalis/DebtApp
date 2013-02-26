f62247deefecd56e9f758f2714079831fcb5b6ae
stvermas <stefaanvermassen@gmail.com>
2013-02-26 20:19:37 +0100

    Don't open onScreen keyboard when Activity starts!

--- src/eu/pinnoo/debtapp/view/MainActivity.java
+++ src/eu/pinnoo/debtapp/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +19,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         //TableLayout t1;
         TableLayout tl = (TableLayout) findViewById(R.id.main_table);
         TableRow tr_head = new TableRow(this);
@@ -27,21 +30,21 @@ public class MainActivity extends Activity {
                 LayoutParams.MATCH_PARENT,
                 LayoutParams.WRAP_CONTENT));
         TextView label_amount = new TextView(this);
-         label_amount.setId(20);
-         label_amount.setText("5");
-         label_amount.setTextColor(Color.WHITE);
-         label_amount.setPadding(5, 5, 5, 5);
-         tr_head.addView(label_amount);// add the column to the table row here
+        label_amount.setId(20);
+        label_amount.setText("5");
+        label_amount.setTextColor(Color.WHITE);
+        label_amount.setPadding(5, 5, 5, 5);
+        tr_head.addView(label_amount);// add the column to the table row here
 
-         TextView label_description = new TextView(this);
-         label_description.setId(21);// define id that must be unique
-         label_description.setText("Pizza"); // set the text for the header 
-         label_description.setTextColor(Color.WHITE); // set the color
-         label_description.setPadding(5, 5, 5, 5); // set the padding (if required)
-         tr_head.addView(label_description); // add the column to the table row here
-         tl.addView(tr_head, new TableLayout.LayoutParams(
-                 LayoutParams.MATCH_PARENT,
-                 LayoutParams.WRAP_CONTENT));
+        TextView label_description = new TextView(this);
+        label_description.setId(21);// define id that must be unique
+        label_description.setText("Pizza"); // set the text for the header 
+        label_description.setTextColor(Color.WHITE); // set the color
+        label_description.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr_head.addView(label_description); // add the column to the table row here
+        tl.addView(tr_head, new TableLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT));
 
     }
 }
