381bedd185cbce0ee9c2b56fa672ca7be9f0f802
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-03-02 17:02:22 +0100

    typo

--- src/eu/pinnoo/debtapp/view/MainActivity.java
+++ src/eu/pinnoo/debtapp/view/MainActivity.java
@@ -312,7 +312,7 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPreExecute() {
-            Dialog.setMessage("Verifing password...");
+            Dialog.setMessage("Verifying password...");
             Dialog.show();
         }
 
