80289a2b57d4421f85e6416243d145433116c278
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-03-05 00:52:48 +0100

    Added description & amount field; added a listview for the debtors instead of checkboxes; but can't get the OK-button back on the screen

--- src/eu/pinnoo/debtapp/view/SplitthebillActivity.java
+++ src/eu/pinnoo/debtapp/view/SplitthebillActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import eu.pinnoo.debtapp.Debt;
@@ -20,39 +21,43 @@ import java.util.List;
  *
  * @author Eveline Hoogstoel <eveline.hoogstoel@ugent.be>
  */
-public class SplitthebillActivity extends Activity{
-    
+public class SplitthebillActivity extends Activity {
+
     private List<User> userlist;
-    
+
     @Override
-    public void onCreate(Bundle savendInstanceState){
+    public void onCreate(Bundle savendInstanceState) {
         super.onCreate(savendInstanceState);
         setContentView(R.layout.split_the_bill);
         final TextView payerlabel = (TextView) findViewById(R.id.payer);
         payerlabel.setText("Payer"); //of in xml omdat dit toch niet verandert?
         final TextView debtorslabel = (TextView) findViewById(R.id.debtors);
         debtorslabel.setText("Debtors:");
-        
+
         final Spinner spinner = (Spinner) findViewById(R.id.spinner3);
         userlist = DAO.getInstance().getUsers();
-        
+
         UserArrayAdapter adapter = new UserArrayAdapter(this, userlist);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(adapter);
-            
-        final LinearLayout ll = (LinearLayout) findViewById(R.id.debtorslist);
-        for(User user: userlist){
-            CheckBox ch = new CheckBox(this);
-            ch.setText(user.getName());
-            ll.addView(ch);
-        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        final ListView lv = (ListView) findViewById(R.id.debtorslist);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, android.R.id.text1, getUserList()));
     }
-    
-    public void splitTheBill(Debt debt, User payer, User[] users){
+
+    public String[] getUserList() {
+        String[] list = new String[userlist.size()];
+        for (int i = 0; i < userlist.size(); i++) {
+            list[i] = userlist.get(i).getName();
+        }
+        return list;
+    }
+
+    public void splitTheBill(Debt debt, User payer, User[] users) {
         int debtors = users.length;
-        Debt splited = new Debt(debt.getAmount()/debtors, debt.getDescription());
-        for(int i=0; i < users.length; i++){
-            if(!users[i].equals(payer)){
+        Debt splited = new Debt(debt.getAmount() / debtors, debt.getDescription());
+        for (int i = 0; i < users.length; i++) {
+            if (!users[i].equals(payer)) {
                 //toevoegen aan DB (TODO)
                 //dao.addDebt(payer,users[i],splited);
             }
@@ -78,9 +83,8 @@ public class SplitthebillActivity extends Activity{
                 return super.onOptionsItemSelected(item);
         }
     }
-    
-    public void refresh(){
+
+    public void refresh() {
         // TODO: everything that needs to refresh when there's an update in the DB, must be refreshed here
     }
-    
 }
