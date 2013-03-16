22918bbf98b435860ec9856199f7e1292381fef8
Wouter Pinnoo <pinnoo.wouter@gmail.com>
2013-03-16 14:35:51 +0100

    update webinterface

--- webinterface/web.php
+++ webinterface/web.php
@@ -1,16 +1,34 @@
 <?php
+
+error_reporting(E_ALL);
+
 $password=trim($_POST[password]);
 $debtor=trim($_POST[debtor]);
 $creditor=trim($_POST[creditor]);
 $amount=trim($_POST[amount]);
 $description=trim($_POST[description]);
-if($password == "XXXXXXXXX"){
-	mysql_connect("localhost","user","XXXXXXXXXXX");
-	mysql_select_db("debtdb");
-	$sql=mysql_query('INSERT INTO Debts(amount, description, creditorid, debtorid) VALUES($amount , $description , (SELECT id FROM User WHERE name=$creditor) , (SELECT id FROM User WHERE=$debtor))');
-	while($row=mysql_fetch_assoc($sql))
-	$output[]=$row;
-	print(json_encode($output));
+
+if (sha1($password) == "XXXXXXXXXXXXXXX") {
+
+	$host = "localhost";
+	$username = "user";
+	$error1 = "error1";
+	$error1 = "error2";
+
+	$db = mysql_connect($host, $username, $password) or die ($error1);
+
+	$dbnaam = "debtdb";
+
+	if(!(@mysql_select_db($dbnaam, $db) or die ($error1))) {
+		print $error1;
+		exit;
+	}
+
+	$stmt="INSERT INTO Debts(amount, description, creditorid, debtorid) VALUES($amount, '$description', (SELECT id FROM User WHERE name='$creditor'), (SELECT id FROM User WHERE name='$debtor'))";
+	print $stmt . "<br />";
+
+	mysql_query($stmt);
+
 	mysql_close();
 }
 ?>
